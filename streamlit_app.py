97f46347b60b7bae2508893e51beff3a735952fc
namanmuktha <109080960+namanmuktha@users.noreply.github.com>
2023-12-30 23:21:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,65 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+# app.py
+
 import streamlit as st
+from model_deploy import load_model, predict
 
-"""
-# Welcome to Streamlit!
+def get_mental_health_condition(prediction):
+    # Mapping of mental health conditions
+    mental_health_mapping = {
+        0: 'ADHD',
+        1: 'ASD',
+        2: 'Loneliness',
+        3: 'MDD',
+        4: 'OCD',
+        5: 'PDD',
+        6: 'PTSD',
+        7: 'anxiety',
+        8: 'bipolar',
+        9: 'eating disorder',
+        10: 'psychotic depression',
+        11: 'sleeping disorder'
+    }
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Get the corresponding mental health condition for the predicted key
+    return mental_health_mapping.get(prediction, 'Unknown')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def main():
+    st.title("Mental Health Prediction")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    # Load the model
+    model = load_model()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    # Create input form for each feature
+    features = [
+        'feeling.nervous', 'panic', 'breathing.rapidly', 'sweating',
+        'trouble.in.concentration', 'having.trouble.in.sleeping',
+        'having.trouble.with.work', 'hopelessness', 'anger', 'over.react',
+        'change.in.eating', 'suicidal.thought', 'feeling.tired', 'close.friend',
+        'social.media.addiction', 'weight.gain', 'introvert',
+        'popping.up.stressful.memory', 'having.nightmares',
+        'avoids.people.or.activities', 'feeling.negative',
+        'trouble.concentrating', 'blaming.yourself', 'hallucinations',
+        'repetitive.behavior', 'increased.energy', 'age'
+    ]
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    user_inputs = {}
+    for feature in features:
+        if feature == 'age':
+            user_inputs[feature] = st.number_input(f"Age: Enter your age", min_value=0, max_value=150, step=1, value=30)
+        else:
+            user_inputs[feature] = st.radio(f"{feature.capitalize()}: Select 'Yes' or 'No'", ["Yes", "No"])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    if st.button("Predict"):
+        # Convert user input to binary (1 for 'Yes', 0 for 'No')
+        input_data = [1 if user_inputs[feature] == "Yes" else 0 if user_inputs[feature] == "No" else user_inputs[feature] for feature in features]
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+        # Perform prediction using the loaded model
+        prediction = predict(model, [input_data])[0]
+
+        # Get the corresponding mental health condition for the predicted key
+        predicted_condition = get_mental_health_condition(prediction)
+
+        st.write("Predicted Mental Health Condition:", predicted_condition)
+
+if __name__ == "__main__":
+    main()
